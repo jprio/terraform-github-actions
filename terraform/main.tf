81e547306c9345a1fccf0b59adfedea8b5e37617
Jean-Pierre RIO <jeanpierre.rio@gmail.com>
2024-11-29 17:48:23 +0000

    _

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,24 @@
 terraform {
- required_providers {
-   aws = {
-     source = "hashicorp/aws"
-   }
- }
- 
- backend "s3" {
-   region = "us-west-2"
-   key    = "terraform.tfstate"
- }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    region = "us-west-2"
+    key    = "terraform.tfstate"
+  }
 }
- 
+
 provider "aws" {
- region = "us-west-2"
+  region = "us-west-2"
 }
- 
+
 resource "aws_instance" "test_instance1" {
- ami           = "ami-830c94e3"
- instance_type = "t2.nano"
- tags = {
-   Name = "test_instance"
- }
+  ami           = "ami-830c94e3"
+  instance_type = "t2.nano"
+  tags = {
+    Name = "test_instance"
+  }
 }
